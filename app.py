aaa21d86dd7e699463b6ec420a96167a467bb768
Varunaa23 <teckchandanivaruna@gmail.com>
2025-03-21 16:06:58 +0530

    Message:3

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return render_template('index.html', message="Hello, Flask!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=80)
